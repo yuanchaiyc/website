3b3521ddbe8726b42a32b2905a88d7c49d41be66
yuanchaiyc <cyjy2017@gmail.com>
2026-02-19 11:37:09 -0800

    update more

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:/Github/website")
+setwd("C:/Work/Github/website")
 
 #render your sweet site. 
 rmarkdown::render_site()
